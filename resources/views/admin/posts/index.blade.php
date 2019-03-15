d7f057acff05dce57f540efdba20482105f5e8be
bouthot <bouthot33@gmail.com>
2019-03-15 11:06:25 -0400

    19th commit - S28 L238 still cant get create post photo to work

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -28,11 +28,10 @@
           <tr>
               <td>{{$post->id}}</td>
               <td><img height="50" src="{{$post->photo ? $post->photo->file : 'no file'}}" alt=""></td>
-              <td><a href="{{route('admin.posts.edit', $post->id)}}">{{$post->name}}</a></td>
-              <td>{{$post->user->name}}</td>
-              <td>{{$post->category ? $post->category->name : 'not categorized'}}</td>
+              <td><a href="{{route('admin.posts.edit', $post->id)}}">{{$post->user->name}}</a></td>
+              <td>{{$post->category ? $post->category->name : 'Uncategorized'}}</td>
               <td>{{$post->title}}</td>
-              <td>{{$post->body}}</td>
+              <td>{{str_limit($post->body, 30)}}</td>
               <td>{{$post->created_at}}</td>
               <td>{{$post->updated_at->diffForHumans()}}</td>
           </tr>
